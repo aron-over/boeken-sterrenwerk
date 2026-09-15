b54e8c336696ac8f0e173b7d91eaaa66cb192f74
Aron Overgaauw <aronovergaauw@gmail.com>
2026-09-15 21:31:31 +0200

    Prijsbadge verwijderd uit samenvatting Alle boeken; alleen zichtbaar bij bewerken

--- serve.ps1
+++ serve.ps1
@@ -1,4 +1,4 @@
-$port = 8080
+param([int]$port = 8080)
 $listener = New-Object System.Net.HttpListener
 $listener.Prefixes.Add("http://localhost:$port/")
 try {
@@ -13,49 +13,55 @@ $baseDir = $PSScriptRoot
 
 try {
     while ($listener.IsListening) {
-        $context = $listener.GetContext()
-        $request = $context.Request
-        $response = $context.Response
+        try {
+            $context = $listener.GetContext()
+            $request = $context.Request
+            $response = $context.Response
 
-        $localPath = $request.Url.LocalPath
-        if ($localPath -eq "/" -or [string]::IsNullOrWhiteSpace($localPath)) {
-            $localPath = "/index.html"
-        }
-
-        $relPath = $localPath.TrimStart('/').Replace('/', [System.IO.Path]::DirectorySeparatorChar)
-        $filePath = [System.IO.Path]::Combine($baseDir, $relPath)
-
-        if ([System.IO.File]::Exists($filePath)) {
-            $ext = [System.IO.Path]::GetExtension($filePath).ToLower()
-            $contentType = switch ($ext) {
-                ".html" { "text/html; charset=utf-8" }
-                ".css"  { "text/css; charset=utf-8" }
-                ".js"   { "application/javascript; charset=utf-8" }
-                ".json" { "application/json; charset=utf-8" }
-                ".png"  { "image/png" }
-                ".jpg"  { "image/jpeg" }
-                ".jpeg" { "image/jpeg" }
-                ".gif"  { "image/gif" }
-                ".svg"  { "image/svg+xml" }
-                ".ico"  { "image/x-icon" }
-                default { "application/octet-stream" }
+            $localPath = $request.Url.LocalPath
+            if ($localPath -eq "/" -or [string]::IsNullOrWhiteSpace($localPath)) {
+                $localPath = "/index.html"
             }
 
-            $bytes = [System.IO.File]::ReadAllBytes($filePath)
-            $response.ContentType = $contentType
-            $response.ContentLength64 = $bytes.Length
-            $response.AddHeader("Access-Control-Allow-Origin", "*")
-            $response.AddHeader("Cache-Control", "no-cache")
-            $response.OutputStream.Write($bytes, 0, $bytes.Length)
-        } else {
-            $response.StatusCode = 404
-            $msg = [System.Text.Encoding]::UTF8.GetBytes("404 Not Found")
-            $response.ContentType = "text/plain; charset=utf-8"
-            $response.ContentLength64 = $msg.Length
-            $response.OutputStream.Write($msg, 0, $msg.Length)
-        }
+            $relPath = $localPath.TrimStart('/').Replace('/', [System.IO.Path]::DirectorySeparatorChar)
+            $filePath = [System.IO.Path]::Combine($baseDir, $relPath)
 
-        $response.Close()
+            if ([System.IO.File]::Exists($filePath)) {
+                $ext = [System.IO.Path]::GetExtension($filePath).ToLower()
+                $contentType = switch ($ext) {
+                    ".html" { "text/html; charset=utf-8" }
+                    ".css"  { "text/css; charset=utf-8" }
+                    ".js"   { "application/javascript; charset=utf-8" }
+                    ".json" { "application/json; charset=utf-8" }
+                    ".png"  { "image/png" }
+                    ".jpg"  { "image/jpeg" }
+                    ".jpeg" { "image/jpeg" }
+                    ".gif"  { "image/gif" }
+                    ".svg"  { "image/svg+xml" }
+                    ".ico"  { "image/x-icon" }
+                    default { "application/octet-stream" }
+                }
+
+                $bytes = [System.IO.File]::ReadAllBytes($filePath)
+                $response.ContentType = $contentType
+                $response.AddHeader("Access-Control-Allow-Origin", "*")
+                $response.AddHeader("Cache-Control", "no-cache")
+                $response.ContentLength64 = $bytes.LongLength
+                $response.OutputStream.Write($bytes, 0, $bytes.Length)
+            } else {
+                $response.StatusCode = 404
+                $msg = [System.Text.Encoding]::UTF8.GetBytes("404 Not Found")
+                $response.ContentType = "text/plain; charset=utf-8"
+                $response.ContentLength64 = $msg.LongLength
+                $response.OutputStream.Write($msg, 0, $msg.Length)
+            }
+            $response.Close()
+        } catch {
+            Write-Host "Request fout: $_"
+            if ($response) {
+                try { $response.Abort() } catch {}
+            }
+        }
     }
 } finally {
     $listener.Stop()
